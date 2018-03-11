a79b409c36413b71373a32c3ac381c3d354544b1
Sankarsan Kampa <me@sankarsankampa.com>
2018-03-11 01:43:29 +0000

    Cleaned up for rewrite

--- assets/scripts/main.js
+++
@@ -1,8 +0,0 @@
-function calculateXP(btn) {
-  let level = btn.form.elements['level'].value;
-  level = parseInt(level);
-
-  if (level >= 0) {
-    btn.form.elements['xp'].value = Math.floor((level / 0.15) * (level / 0.15));
-  }
-}
